a60ba12c82a077edf9c30f51fa7ede66fd9a8640
Eduardo Stegani <estegani@vidalink.com.br>
2026-09-12 22:15:43 -0300

    initialize project churchhub web

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/**
+ * IgrejaHub
+ * JavaScript principal da aplicação
+ */
+
+console.log("IgrejaHub iniciado com sucesso!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Página carregada!");
+});
